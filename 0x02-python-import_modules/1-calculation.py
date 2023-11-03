680a35c8d0d554b46f470c8c676fc52c827b043d
Geitodyu Henrique Crusoe <crusoehenrique3@gmail.com>
2023-11-03 03:50:10 +0300

    advance through

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "_main_":
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, add(a, b)))
+    print("{} * {} = {}".format(a, b, add(a, b)))
+    print("{} / {} = {}".format(a, b, add(a, b)))
