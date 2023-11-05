39e597b1e9d499d71f7ef7e39d96f9630b06671b
Geitodyu Henrique Crusoe <crusoehenrique3@gmail.com>
2023-11-05 21:35:36 +0300

    test

---
+++ test/8-multiple_returns.py
@@ -0,0 +1,7 @@
+# A function that returns a tuple with the length of a string and its first character.
+def multiple_returns(sentence):
+    if len(sentence) == 0:
+        first_char = None
+    else:
+        first_char = sentence[0]
+    return (len(sentence), first_char)
